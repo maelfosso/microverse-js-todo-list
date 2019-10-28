ba9cf2ada61211a177977b3829f8c16e88db5b69
Mael FOSSO <fosso.mael.elvis@gmail.com>
2019-10-28 04:45:17 +0100

    Models Event, Todo and Module EventAggregator set up

---
+++ src/models/todo.js
@@ -0,0 +1,9 @@
+export default class todo {
+  constructor(name, description, due, project, prioriy) {
+    this.name = name;
+    this.description = description;
+    this.due = due;
+    this.project = project;
+    this.prioriy = prioriy;
+  }
+}
